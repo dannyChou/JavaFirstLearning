8f4236a5abe35d84d5669590ff960f18f1f665f1
dannychou <danny75@gmail.com>
2019-11-21 14:25:42 +0800

    完成測試案例

--- src/test/java/com/Danny/chapter1/Chapter1ApplicationTests.java
+++ src/test/java/com/Danny/chapter1/Chapter1ApplicationTests.java
@@ -1,13 +1,40 @@
 package com.Danny.chapter1;
 
+import com.Danny.web.HelloController;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.event.annotation.BeforeTestExecution;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 class Chapter1ApplicationTests {
 
+	private MockMvc mvc;
+
+	@BeforeEach
+	public void setUp() throws Exception{
+		mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+	}
+
 	@Test
-	void contextLoads() {
+	public void getHello() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Hello World")));
 	}
 
 }
